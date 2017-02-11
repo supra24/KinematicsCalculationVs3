ac04785b4788ea1edbad14caa4aa0c46a87e0c1d
Jan Nowak <tojota24@gmail.com>
2017-02-11 18:31:08 +0100

    zmiana jezyka

--- app/src/main/java/com/example/damian/kinematicscalculatorvs3/activities/MenuActivity.java
+++ app/src/main/java/com/example/damian/kinematicscalculatorvs3/activities/MenuActivity.java
@@ -1,9 +1,13 @@
 package com.example.damian.kinematicscalculatorvs3.activities;
 
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.view.View;
 import android.widget.Button;
 
 import com.example.damian.kinematicscalculatorvs3.R;
@@ -19,6 +23,7 @@ import org.apache.commons.math3.ode.ParameterJacobianProvider;
 import org.apache.commons.math3.ode.UnknownParameterException;
 
 import java.util.Collection;
+import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -33,6 +38,12 @@ public class MenuActivity extends AppCompatActivity {
     Button b_kinematics_forward;
     @BindView(R.id.b_kinematicsInverse)
     Button b_kinematics_inverse;
+    @BindView(R.id.b_change_language)
+    Button b_change_language;
+    @BindView(R.id.b_language_pl)
+    Button b_pl;
+    @BindView(R.id.b_language_en)
+    Button b_en;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -52,5 +63,34 @@ public class MenuActivity extends AppCompatActivity {
     void openActivityKinematicsInverse(){
         Intent intent = new Intent(getApplicationContext(), KinematicsInverseVariablesConstant.class);
         startActivity(intent);
+    }
+
+    @OnClick(R.id.b_change_language)
+    void changeLanguage(){
+        b_en.setVisibility(View.VISIBLE);
+        b_pl.setVisibility(View.VISIBLE);
+    }
+
+    @OnClick(R.id.b_language_en)
+    void changeLanguageEn(){
+        setLocale("en");
+    }
+
+    @OnClick(R.id.b_language_pl)
+    void changeLanguagePl(){
+        setLocale("pl");
+    }
+
+    public void setLocale(String lang) {
+
+        Locale myLocale;
+        myLocale = new Locale(lang);
+        Resources res = getResources();
+        DisplayMetrics dm = res.getDisplayMetrics();
+        Configuration conf = res.getConfiguration();
+        conf.locale = myLocale;
+        res.updateConfiguration(conf, dm);
+        Intent refresh = new Intent(this, MenuActivity.class);
+        startActivity(refresh);
     }
 }
